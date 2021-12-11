7b09e5ea5af5e6c72ede7346ed7201d2f5b84b7e
Subodh Bijwe <sbijwe1729@gmail.com>
2021-12-11 16:51:30 +0530

    as

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, url_for, redirect, render_template
 import pandas as pd
 import pickle
-from flask_core import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
